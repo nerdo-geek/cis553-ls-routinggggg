fd626d599ef4d3e4b61809a45dbe3647f52880cf
nerdo-geek <109935871+nerdo-geek@users.noreply.github.com>
2025-11-02 18:20:19 -0500

    Update penn-chord-message.cc

--- contrib/upenn-cis553/penn-search/penn-chord-message.cc
+++ contrib/upenn-cis553/penn-search/penn-chord-message.cc
@@ -22,13 +22,8 @@ using namespace ns3;
 NS_LOG_COMPONENT_DEFINE ("PennChordMessage");
 NS_OBJECT_ENSURE_REGISTERED (PennChordMessage);
 
-PennChordMessage::PennChordMessage ()
-{
-}
-
-PennChordMessage::~PennChordMessage ()
-{
-}
+PennChordMessage::PennChordMessage () {}
+PennChordMessage::~PennChordMessage () {}
 
 PennChordMessage::PennChordMessage (PennChordMessage::MessageType messageType, uint32_t transactionId)
 {
@@ -52,22 +47,33 @@ PennChordMessage::GetInstanceTypeId (void) const
   return GetTypeId ();
 }
 
+static inline void WriteIpv4 (Buffer::Iterator &i, Ipv4Address a)
+{
+  uint32_t raw = a.Get ();
+  i.WriteHtonU32 (raw);
+}
+static inline Ipv4Address ReadIpv4 (Buffer::Iterator &i)
+{
+  return Ipv4Address (i.ReadNtohU32 ());
+}
+
+/* ===== Core Header ===== */
 
 uint32_t
 PennChordMessage::GetSerializedSize (void) const
 {
-  // size of messageType, transaction id
   uint32_t size = sizeof (uint8_t) + sizeof (uint32_t);
   switch (m_messageType)
     {
-      case PING_REQ:
-        size += m_message.pingReq.GetSerializedSize ();
-        break;
-      case PING_RSP:
-        size += m_message.pingRsp.GetSerializedSize ();
-        break;
-      default:
-        NS_ASSERT (false);
+      case PING_REQ:   size += m_message.pingReq.GetSerializedSize (); break;
+      case PING_RSP:   size += m_message.pingRsp.GetSerializedSize (); break;
+      case JOIN_FIND:  size += m_message.joinFind.GetSerializedSize (); break;
+      case JOIN_REPLY: size += m_message.joinReply.GetSerializedSize (); break;
+      case STAB_REQ:   size += m_message.stabReq.GetSerializedSize (); break;
+      case STAB_RSP:   size += m_message.stabRsp.GetSerializedSize (); break;
+      case NOTIFY:     size += m_message.notify.GetSerializedSize (); break;
+      case RINGSTATE:  size += m_message.ringstate.GetSerializedSize (); break;
+      default: NS_ASSERT (false);
     }
   return size;
 }
@@ -76,20 +82,20 @@ void
 PennChordMessage::Print (std::ostream &os) const
 {
   os << "\n****PennChordMessage Dump****\n" ;
-  os << "messageType: " << m_messageType << "\n";
+  os << "messageType: " << (uint32_t)m_messageType << "\n";
   os << "transactionId: " << m_transactionId << "\n";
   os << "PAYLOAD:: \n";
-  
   switch (m_messageType)
     {
-      case PING_REQ:
-        m_message.pingReq.Print (os);
-        break;
-      case PING_RSP:
-        m_message.pingRsp.Print (os);
-        break;
-      default:
-        break;  
+      case PING_REQ:   m_message.pingReq.Print (os); break;
+      case PING_RSP:   m_message.pingRsp.Print (os); break;
+      case JOIN_FIND:  m_message.joinFind.Print (os); break;
+      case JOIN_REPLY: m_message.joinReply.Print (os); break;
+      case STAB_REQ:   m_message.stabReq.Print (os); break;
+      case STAB_RSP:   m_message.stabRsp.Print (os); break;
+      case NOTIFY:     m_message.notify.Print (os); break;
+      case RINGSTATE:  m_message.ringstate.Print (os); break;
+      default: break;
     }
   os << "\n****END OF MESSAGE****\n";
 }
@@ -103,14 +109,15 @@ PennChordMessage::Serialize (Buffer::Iterator start) const
 
   switch (m_messageType)
     {
-      case PING_REQ:
-        m_message.pingReq.Serialize (i);
-        break;
-      case PING_RSP:
-        m_message.pingRsp.Serialize (i);
-        break;
-      default:
-        NS_ASSERT (false);   
+      case PING_REQ:   m_message.pingReq.Serialize (i); break;
+      case PING_RSP:   m_message.pingRsp.Serialize (i); break;
+      case JOIN_FIND:  m_message.joinFind.Serialize (i); break;
+      case JOIN_REPLY: m_message.joinReply.Serialize (i); break;
+      case STAB_REQ:   m_message.stabReq.Serialize (i); break;
+      case STAB_RSP:   m_message.stabRsp.Serialize (i); break;
+      case NOTIFY:     m_message.notify.Serialize (i); break;
+      case RINGSTATE:  m_message.ringstate.Serialize (i); break;
+      default: NS_ASSERT (false);
     }
 }
 
@@ -121,154 +128,89 @@ PennChordMessage::Deserialize (Buffer::Iterator start)
   Buffer::Iterator i = start;
   m_messageType = (MessageType) i.ReadU8 ();
   m_transactionId = i.ReadNtohU32 ();
-
   size = sizeof (uint8_t) + sizeof (uint32_t);
 
   switch (m_messageType)
     {
-      case PING_REQ:
-        size += m_message.pingReq.Deserialize (i);
-        break;
-      case PING_RSP:
-        size += m_message.pingRsp.Deserialize (i);
-        break;
-      default:
-        NS_ASSERT (false);
+      case PING_REQ:   size += m_message.pingReq.Deserialize (i); break;
+      case PING_RSP:   size += m_message.pingRsp.Deserialize (i); break;
+      case JOIN_FIND:  size += m_message.joinFind.Deserialize (i); break;
+      case JOIN_REPLY: size += m_message.joinReply.Deserialize (i); break;
+      case STAB_REQ:   size += m_message.stabReq.Deserialize (i); break;
+      case STAB_RSP:   size += m_message.stabRsp.Deserialize (i); break;
+      case NOTIFY:     size += m_message.notify.Deserialize (i); break;
+      case RINGSTATE:  size += m_message.ringstate.Deserialize (i); break;
+      default: NS_ASSERT (false);
     }
   return size;
 }
 
-/* PING_REQ */
+/* ===== PING_REQ ===== */
+uint32_t PennChordMessage::PingReq::GetSerializedSize () const { return sizeof(uint16_t) + pingMessage.length(); }
+void     PennChordMessage::PingReq::Print (std::ostream &os) const { os << "PingReq:: Message: " << pingMessage << "\n"; }
+void     PennChordMessage::PingReq::Serialize (Buffer::Iterator &i) const { i.WriteU16 (pingMessage.length()); i.Write ((uint8_t*)(const_cast<char*>(pingMessage.c_str())), pingMessage.length()); }
+uint32_t PennChordMessage::PingReq::Deserialize (Buffer::Iterator &i) { uint16_t l=i.ReadU16(); char* s=(char*)malloc(l); i.Read((uint8_t*)s,l); pingMessage=std::string(s,l); free(s); return GetSerializedSize(); }
+void     PennChordMessage::SetPingReq (std::string m) { if (m_messageType==0) m_messageType=PING_REQ; else NS_ASSERT (m_messageType==PING_REQ); m_message.pingReq.pingMessage=m; }
+PennChordMessage::PingReq PennChordMessage::GetPingReq () { return m_message.pingReq; }
 
-uint32_t 
-PennChordMessage::PingReq::GetSerializedSize (void) const
-{
-  uint32_t size;
-  size = sizeof(uint16_t) + pingMessage.length();
-  return size;
-}
+/* ===== PING_RSP ===== */
+uint32_t PennChordMessage::PingRsp::GetSerializedSize () const { return sizeof(uint16_t) + pingMessage.length(); }
+void     PennChordMessage::PingRsp::Print (std::ostream &os) const { os << "PingReq:: Message: " << pingMessage << "\n"; }
+void     PennChordMessage::PingRsp::Serialize (Buffer::Iterator &i) const { i.WriteU16 (pingMessage.length()); i.Write ((uint8_t*)(const_cast<char*>(pingMessage.c_str())), pingMessage.length()); }
+uint32_t PennChordMessage::PingRsp::Deserialize (Buffer::Iterator &i) { uint16_t l=i.ReadU16(); char* s=(char*)malloc(l); i.Read((uint8_t*)s,l); pingMessage=std::string(s,l); free(s); return GetSerializedSize(); }
+void     PennChordMessage::SetPingRsp (std::string m) { if (m_messageType==0) m_messageType=PING_RSP; else NS_ASSERT (m_messageType==PING_RSP); m_message.pingRsp.pingMessage=m; }
+PennChordMessage::PingRsp PennChordMessage::GetPingRsp () { return m_message.pingRsp; }
 
-void
-PennChordMessage::PingReq::Print (std::ostream &os) const
-{
-  os << "PingReq:: Message: " << pingMessage << "\n";
-}
+/* ===== JOIN_FIND ===== */
+uint32_t PennChordMessage::JoinFind::GetSerializedSize () const { return IPV4_ADDRESS_SIZE + sizeof(uint32_t) + IPV4_ADDRESS_SIZE; }
+void     PennChordMessage::JoinFind::Print (std::ostream &os) const { os << "JoinFind:: joiner=" << joiner << " hash=" << joinerHash << " origin=" << origin << "\n"; }
+void     PennChordMessage::JoinFind::Serialize (Buffer::Iterator &i) const { WriteIpv4(i, joiner); i.WriteHtonU32(joinerHash); WriteIpv4(i, origin); }
+uint32_t PennChordMessage::JoinFind::Deserialize (Buffer::Iterator &i) { joiner=ReadIpv4(i); joinerHash=i.ReadNtohU32(); origin=ReadIpv4(i); return GetSerializedSize(); }
+void     PennChordMessage::SetJoinFind (Ipv4Address joiner, uint32_t joinerHash, Ipv4Address origin) { if (m_messageType==0) m_messageType=JOIN_FIND; else NS_ASSERT(m_messageType==JOIN_FIND); m_message.joinFind.joiner=joiner; m_message.joinFind.joinerHash=joinerHash; m_message.joinFind.origin=origin; }
+PennChordMessage::JoinFind PennChordMessage::GetJoinFind () { return m_message.joinFind; }
 
-void
-PennChordMessage::PingReq::Serialize (Buffer::Iterator &start) const
-{
-  start.WriteU16 (pingMessage.length ());
-  start.Write ((uint8_t *) (const_cast<char*> (pingMessage.c_str())), pingMessage.length());
-}
+/* ===== JOIN_REPLY ===== */
+uint32_t PennChordMessage::JoinReply::GetSerializedSize () const { return IPV4_ADDRESS_SIZE; }
+void     PennChordMessage::JoinReply::Print (std::ostream &os) const { os << "JoinReply:: successor=" << successor << "\n"; }
+void     PennChordMessage::JoinReply::Serialize (Buffer::Iterator &i) const { WriteIpv4(i, successor); }
+uint32_t PennChordMessage::JoinReply::Deserialize (Buffer::Iterator &i) { successor=ReadIpv4(i); return GetSerializedSize(); }
+void     PennChordMessage::SetJoinReply (Ipv4Address successor) { if (m_messageType==0) m_messageType=JOIN_REPLY; else NS_ASSERT(m_messageType==JOIN_REPLY); m_message.joinReply.successor=successor; }
+PennChordMessage::JoinReply PennChordMessage::GetJoinReply () { return m_message.joinReply; }
 
-uint32_t
-PennChordMessage::PingReq::Deserialize (Buffer::Iterator &start)
-{  
-  uint16_t length = start.ReadU16 ();
-  char* str = (char*) malloc (length);
-  start.Read ((uint8_t*)str, length);
-  pingMessage = std::string (str, length);
-  free (str);
-  return PingReq::GetSerializedSize ();
-}
+/* ===== STAB_REQ ===== */
+uint32_t PennChordMessage::StabilizeReq::GetSerializedSize () const { return 0; }
+void     PennChordMessage::StabilizeReq::Print (std::ostream &os) const { os << "StabilizeReq\n"; }
+void     PennChordMessage::StabilizeReq::Serialize (Buffer::Iterator &i) const { (void)i; }
+uint32_t PennChordMessage::StabilizeReq::Deserialize (Buffer::Iterator &i) { (void)i; return 0; }
+void     PennChordMessage::SetStabilizeReq () { if (m_messageType==0) m_messageType=STAB_REQ; else NS_ASSERT(m_messageType==STAB_REQ); }
+PennChordMessage::StabilizeReq PennChordMessage::GetStabilizeReq () { return m_message.stabReq; }
 
-void
-PennChordMessage::SetPingReq (std::string pingMessage)
-{
-  if (m_messageType == 0)
-    {
-      m_messageType = PING_REQ;
-    }
-  else
-    {
-      NS_ASSERT (m_messageType == PING_REQ);
-    }
-  m_message.pingReq.pingMessage = pingMessage;
-}
+/* ===== STAB_RSP ===== */
+uint32_t PennChordMessage::StabilizeRsp::GetSerializedSize () const { return IPV4_ADDRESS_SIZE; }
+void     PennChordMessage::StabilizeRsp::Print (std::ostream &os) const { os << "StabilizeRsp:: predecessor=" << predecessor << "\n"; }
+void     PennChordMessage::StabilizeRsp::Serialize (Buffer::Iterator &i) const { WriteIpv4(i, predecessor); }
+uint32_t PennChordMessage::StabilizeRsp::Deserialize (Buffer::Iterator &i) { predecessor=ReadIpv4(i); return GetSerializedSize(); }
+void     PennChordMessage::SetStabilizeRsp (Ipv4Address predecessor) { if (m_messageType==0) m_messageType=STAB_RSP; else NS_ASSERT(m_messageType==STAB_RSP); m_message.stabRsp.predecessor=predecessor; }
+PennChordMessage::StabilizeRsp PennChordMessage::GetStabilizeRsp () { return m_message.stabRsp; }
 
-PennChordMessage::PingReq
-PennChordMessage::GetPingReq ()
-{
-  return m_message.pingReq;
-}
+/* ===== NOTIFY ===== */
+uint32_t PennChordMessage::NotifyMsg::GetSerializedSize () const { return IPV4_ADDRESS_SIZE; }
+void     PennChordMessage::NotifyMsg::Print (std::ostream &os) const { os << "Notify:: candidate=" << candidate << "\n"; }
+void     PennChordMessage::NotifyMsg::Serialize (Buffer::Iterator &i) const { WriteIpv4(i, candidate); }
+uint32_t PennChordMessage::NotifyMsg::Deserialize (Buffer::Iterator &i) { candidate=ReadIpv4(i); return GetSerializedSize(); }
+void     PennChordMessage::SetNotify (Ipv4Address candidate) { if (m_messageType==0) m_messageType=NOTIFY; else NS_ASSERT(m_messageType==NOTIFY); m_message.notify.candidate=candidate; }
+PennChordMessage::NotifyMsg PennChordMessage::GetNotify () { return m_message.notify; }
 
-/* PING_RSP */
+/* ===== RINGSTATE ===== */
+uint32_t PennChordMessage::RingStateMsg::GetSerializedSize () const { return IPV4_ADDRESS_SIZE; }
+void     PennChordMessage::RingStateMsg::Print (std::ostream &os) const { os << "RingState:: origin=" << origin << "\n"; }
+void     PennChordMessage::RingStateMsg::Serialize (Buffer::Iterator &i) const { WriteIpv4(i, origin); }
+uint32_t PennChordMessage::RingStateMsg::Deserialize (Buffer::Iterator &i) { origin=ReadIpv4(i); return GetSerializedSize(); }
+void     PennChordMessage::SetRingState (Ipv4Address origin) { if (m_messageType==0) m_messageType=RINGSTATE; else NS_ASSERT(m_messageType==RINGSTATE); m_message.ringstate.origin=origin; }
+PennChordMessage::RingStateMsg PennChordMessage::GetRingState () { return m_message.ringstate; }
 
-uint32_t 
-PennChordMessage::PingRsp::GetSerializedSize (void) const
-{
-  uint32_t size;
-  size = sizeof(uint16_t) + pingMessage.length();
-  return size;
-}
-
-void
-PennChordMessage::PingRsp::Print (std::ostream &os) const
-{
-  os << "PingReq:: Message: " << pingMessage << "\n";
-}
-
-void
-PennChordMessage::PingRsp::Serialize (Buffer::Iterator &start) const
-{
-  start.WriteU16 (pingMessage.length ());
-  start.Write ((uint8_t *) (const_cast<char*> (pingMessage.c_str())), pingMessage.length());
-}
-
-uint32_t
-PennChordMessage::PingRsp::Deserialize (Buffer::Iterator &start)
-{  
-  uint16_t length = start.ReadU16 ();
-  char* str = (char*) malloc (length);
-  start.Read ((uint8_t*)str, length);
-  pingMessage = std::string (str, length);
-  free (str);
-  return PingRsp::GetSerializedSize ();
-}
-
-void
-PennChordMessage::SetPingRsp (std::string pingMessage)
-{
-  if (m_messageType == 0)
-    {
-      m_messageType = PING_RSP;
-    }
-  else
-    {
-      NS_ASSERT (m_messageType == PING_RSP);
-    }
-  m_message.pingRsp.pingMessage = pingMessage;
-}
-
-PennChordMessage::PingRsp
-PennChordMessage::GetPingRsp ()
-{
-  return m_message.pingRsp;
-}
-
-
-
-void
-PennChordMessage::SetMessageType (MessageType messageType)
-{
-  m_messageType = messageType;
-}
-
-PennChordMessage::MessageType
-PennChordMessage::GetMessageType () const
-{
-  return m_messageType;
-}
-
-void
-PennChordMessage::SetTransactionId (uint32_t transactionId)
-{
-  m_transactionId = transactionId;
-}
-
-uint32_t 
-PennChordMessage::GetTransactionId (void) const
-{
-  return m_transactionId;
-}
-
+/* ===== Core setters ===== */
+void PennChordMessage::SetMessageType (MessageType t) { m_messageType = t; }
+PennChordMessage::MessageType PennChordMessage::GetMessageType () const { return m_messageType; }
+void PennChordMessage::SetTransactionId (uint32_t id) { m_transactionId = id; }
+uint32_t PennChordMessage::GetTransactionId () const { return m_transactionId; }
